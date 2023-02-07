bbb1b5256df8c07b412117ddfe8f814474064c8f
wenziepoo <123613375+wenziepoo@users.noreply.github.com>
2023-02-07 12:17:14 -0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 def on_button_pressed_a():
-    global counter
+    global counter2
     counter2 = [0, 0]
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_received_string(receivedString):
-    if choice == "A":
-        if receivedString == "C":
-            counter[1] = counter[1] + 1
-        elif receivedString != choice:
-            counter[0] = counter[0] + 1
-    elif choice == "B":
-        if receivedString == "A":
-            counter[1] = counter[1] + 1
-        elif receivedString != choice:
-            counter[0] = counter[0] + 1
-    elif choice == "C":
-        if receivedString == "B":
-            counter[1] = counter[1] + 1
-        elif receivedString != choice:
-            counter[0] = counter[0] + 1
+    if can == True:
+        if choice == "A":
+            if receivedString == "C":
+                counter[1] = counter[1] + 1
+            elif receivedString != choice:
+                counter[0] = counter[0] + 1
+        elif choice == "B":
+            if receivedString == "A":
+                counter[1] = counter[1] + 1
+            elif receivedString != choice:
+                counter[0] = counter[0] + 1
+        elif choice == "C":
+            if receivedString == "B":
+                counter[1] = counter[1] + 1
+            elif receivedString != choice:
+                counter[0] = counter[0] + 1
+        can = False
 radio.on_received_string(on_received_string)
 
 def on_gesture_shake():
@@ -26,14 +28,25 @@ def on_gesture_shake():
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
 def shake():
-    global choice, score, counter
+    global choice, score, can2
     choice = items[randint(0, 2)]
     radio.set_group(8)
-    radio.send_string(choice)
-    basic.pause(100)
+    radio.send_string("" + (choice))
+    if choice == 'A':
+        print(1)
+    elif choice == 'B':
+        print(1)
+    else:
+        print(1)
+
     score = "" + str(counter[0]) + " - " + ("" + str(counter[1]))
+    can2 = True
+can2 = False
 score = ""
 choice = ""
+counter2: List[number] = []
+counter: List[number] = []
 items: List[str] = []
+can3 = False
 items = ["A", "B", "C"]
 counter = [0, 0]
--- main.ts
+++ main.ts
@@ -1,46 +1,72 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-    let counter2 = [0, 0]
+input.onButtonPressed(Button.A, function () {
+    counter2 = [0, 0]
 })
-radio.onReceivedString(function on_received_string(receivedString: string) {
-    if (choice == "A") {
-        if (receivedString == "C") {
-            counter[1] = counter[1] + 1
-        } else if (receivedString != choice) {
-            counter[0] = counter[0] + 1
+radio.onReceivedString(function (receivedString) {
+    let can: boolean;
+if (can == true) {
+        if (choice == "A") {
+            if (receivedString == "C") {
+                counter[1] = counter[1] + 1
+            } else if (receivedString != choice) {
+                counter[0] = counter[0] + 1
+            }
+        } else if (choice == "B") {
+            if (receivedString == "A") {
+                counter[1] = counter[1] + 1
+            } else if (receivedString != choice) {
+                counter[0] = counter[0] + 1
+            }
+        } else if (choice == "C") {
+            if (receivedString == "B") {
+                counter[1] = counter[1] + 1
+            } else if (receivedString != choice) {
+                counter[0] = counter[0] + 1
+            }
         }
-        
-    } else if (choice == "B") {
-        if (receivedString == "A") {
-            counter[1] = counter[1] + 1
-        } else if (receivedString != choice) {
-            counter[0] = counter[0] + 1
-        }
-        
-    } else if (choice == "C") {
-        if (receivedString == "B") {
-            counter[1] = counter[1] + 1
-        } else if (receivedString != choice) {
-            counter[0] = counter[0] + 1
-        }
-        
+        can = false
     }
-    
 })
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
+input.onGesture(Gesture.Shake, function () {
     shake()
 })
-function shake() {
-    
+function shake () {
     choice = items[randint(0, 2)]
     radio.setGroup(8)
-    radio.sendString(choice)
-    basic.pause(100)
-    score = "" + ("" + counter[0]) + " - " + ("" + ("" + counter[1]))
+    radio.sendString("" + (choice))
+    if (choice == "A") {
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            . # # # #
+            . . # # .
+            `)
+    } else if (choice == "B") {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            . # . . #
+            # # # # .
+            . # . . .
+            `)
+    }
+    score = "" + counter[0] + " - " + ("" + counter[1])
+    can2 = true
 }
-
+let can2 = false
 let score = ""
 let choice = ""
-let items : string[] = []
+let counter2: number[] = []
+let counter: number[] = []
+let items: string[] = []
+let can3 = false
 items = ["A", "B", "C"]
-let counter = [0, 0]
+counter = [0, 0]
